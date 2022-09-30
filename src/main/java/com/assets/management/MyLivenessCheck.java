8b3377e920509ba7b560f0e09448d84b9fb9ec47
quarkusio <no-reply@quarkus.io>
2022-09-30 16:02:33 +0000

    Initial commit

---
+++ src/main/java/com/assets/management/MyLivenessCheck.java
@@ -0,0 +1,15 @@
+package com.assets.management;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class MyLivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("alive");
+    }
+
+}
