1a1cc84d4a78b1b8e10a3a438c6f3fbe5fc6a7c1
Japhet Sebastian <japhet.sebastian01@gmail.com>
2023-03-06 00:21:14 +0300

    creates QR code for electronic asset microservice

--- src/main/java/com/assets/management/qrcode/rest/QRGeneratorResource.java
+++ src/main/java/com/assets/management/qrcode/rest/QRGeneratorResource.java
@@ -1,12 +1,18 @@
-package com.assets.management.qrcode.service;
+package com.assets.management.qrcode.rest;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.util.Hashtable;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.jboss.logging.Logger;
 
@@ -18,33 +24,34 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
-@ApplicationScoped
-public class QrService {
+@Path("/generates")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class QRGeneratorResource {
 
 	@Inject
 	Logger LOG;
 
-	public byte[] generateQrString(@Valid QrContent content)
-			throws WriterException, IOException {
-		LOG.info("Content for creating QR: " + content);
+	@POST
+	@Path("/qrcode")
+	public Response generateQrString(@Valid URI collectionOrTransferURI) throws WriterException, IOException {
+		LOG.info("Content for creating QR: " + collectionOrTransferURI);
 		ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
 		Hashtable<EncodeHintType, Object> hintMap = new Hashtable<>();
 		hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
 		hintMap.put(EncodeHintType.MARGIN, 1);
 		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-		
-		BitMatrix bitMatrix = new QRCodeWriter().encode(
-				String.valueOf(content), BarcodeFormat.QR_CODE, 150, 150,
-				hintMap
-		);
+
+		BitMatrix bitMatrix = new QRCodeWriter().encode(String.valueOf(collectionOrTransferURI), BarcodeFormat.QR_CODE,
+				150, 150, hintMap);
 
 		MatrixToImageWriter.writeToStream(bitMatrix, "png", pngOutputStream);
 //		String qrString = Base64.getEncoder().encodeToString(
 //				pngOutputStream.toByteArray()
 //		);
-		
+
 		// TODO: return byte[] then convert to string in electronic-asset client service
-		LOG.info("Returnd QR String: " + content);
-		return pngOutputStream.toByteArray();
+//		LOG.info("Returnd QR String: " + collectionOrTransfer);
+		return Response.ok(pngOutputStream.toByteArray()).build();
 	}
 }
